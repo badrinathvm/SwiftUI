7fadec297355e1fe994de18e7ade7ac8b87661b8
Badarinath Venkatnarayansetty <Badarinath_Venkatnaraaynsetty@intuit.com>
2019-10-12 09:52:37 -0700

    Form sections and Navigation components

--- Basics/Basics/ContentView.swift
+++ Basics/Basics/ContentView.swift
@@ -14,7 +14,26 @@ struct ContentView : View {
     // Returns some thing that conforms to view protocol
     // some: It must always be the same kind of view being returned.
     var body: some View {
-        Text("Hello World")
+        // adding a navigation view.
+        NavigationView {
+            // scrolling list of static controls
+            Form {
+                //Segregates in to sections just like table view sections
+                Section {
+                    Text("Hello Swift")
+                }
+                
+                Group{
+                    Text("iOS")
+                    Text("macOS")
+                    Text("tvOS")
+                    Text("watchOS")
+                }
+            }
+            //make sure navigation view components should be added inside the block not outside.
+            .navigationBarTitle(Text("SwiftUI") , displayMode: .inline)
+        }
+      
     }
 }
 
