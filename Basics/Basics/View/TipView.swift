4bc804e7ad0fb49897c29920e90dd00eed25cecd
Badarinath Venkatnarayansetty <Badarinath_Venkatnaraaynsetty@intuit.com>
2019-10-13 15:37:22 -0700

    Added section header and footer and rerranged the sections

--- Basics/Basics/View/TipView.swift
+++ Basics/Basics/View/TipView.swift
@@ -12,24 +12,33 @@ import SwiftUI
 struct TipView:View {
     @State private var checkAmount = ""
     @State private var people = 0
+    @State private var tipPercentage = 0
+    
+    var tipPercentages = [0,10,20,30,40,50]
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                      TextField($checkAmount)
-                        .textContentType(.oneTimeCode)
-                }
-               
-                Section {
-                    Text("$\(checkAmount)")
-                }
-                
-                Section {
+                    
                     Picker(selection: $people, label: Text("Number of People")) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
                     }
+                }
+                
+                Section(header: Text("How much Tip you want to pay ?"), footer: Text("")) {
+                    Picker(selection: $tipPercentage, label: Text("Tip Percentage")) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
                 }
             }
             .navigationBarTitle(Text("Tip View"))
