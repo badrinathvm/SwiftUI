f5e6c3a44b4494726bc6c710a4e936364eff8d91
Badarinath Venkatnarayansetty <Badarinath_Venkatnaraaynsetty@intuit.com>
2019-10-13 18:06:08 -0700

    Added flags and vstack styling

--- Basics/Basics/View/GuessView.swift
+++ Basics/Basics/View/GuessView.swift
@@ -10,6 +10,11 @@ import Foundation
 import SwiftUI
 
 struct GuessView: View {
+    @State private var showingAlert = false
+    
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         //place two text vertcally aligned leading.
 //        VStack(alignment: .leading, spacing: 20.0) {
@@ -34,12 +39,57 @@ struct GuessView: View {
            //LinearGradient(gradient: Gradient(colors: [Color.white, Color.black]), startPoint: .top, endPoint: .bottom)
         
         //Buttons and Images
-        Button(action: {
-            print("Edit Button Tapped")
-        }) {
-            HStack(spacing: 10.0) {
-                Image(systemName: "pencil")
-                Text("Edit")
+        
+//        VStack(alignment: .leading, spacing: 20) {
+//            Button(action: {
+//                print("Edit Button Tapped")
+//            }) {
+//                HStack(spacing: 10.0) {
+//                    Image(systemName: "pencil")
+//                    Text("Edit")
+//                }
+//            }
+//
+//            Button(action: {
+//                //self.showingAlert = true
+//                  Alert.init(title: Text("Hello Swift UI"), message: Text("Content is Awesome:"), dismissButton: .default(Text("OK")))
+//            }) {
+//                Text("Show Alert")
+//            }
+//
+//        }
+        
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            //LinearGradient(gradient: Gradient(colors: [Color.white, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            //LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                VStack {
+                    Text("Tap the flag")
+                      .font(.largeTitle)
+                     .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                }
+            
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                
+                //pushes to the top
+                Spacer()
             }
         }
     }
