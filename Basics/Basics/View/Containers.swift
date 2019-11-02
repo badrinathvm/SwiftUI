0e6fe5ef659eb556ea2d1303e58c90996c7358ee
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2019-11-01 23:38:07 -0700

    Segregating in to smaller view components and grouping them together

--- Basics/Basics/View/Containers.swift
+++ Basics/Basics/View/Containers.swift
@@ -10,16 +10,38 @@ import Foundation
 import SwiftUI
 
 //Key Points : In order to add a border, we need to make use of background property.
+//Segregating in to small sections.
 
 struct Containers: View {
-    var body: some View {
+    
+    private var horizontalView: some View {
+        HStack(alignment: VerticalAlignment.center, spacing: 20) {
+            ForEach(0..<3) { _ in
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(Color.orange)
+                    .frame(width: 100, height: 180)
+            }
+        }.padding()
+            .background(RoundedRectangle(cornerRadius: 20).stroke(Color.orange, lineWidth: 10))
+    }
+    
+    private var verticalView: some View {
         VStack(alignment: .leading, spacing: 40) {
-            ForEach(0..<4) { _ in
+            ForEach(0..<3) { _ in
                 RoundedRectangle(cornerRadius: 20)
                     .foregroundColor(Color.orange)
                     .frame(width: 320, height: 150)
             }
         }.padding()
             .background(RoundedRectangle(cornerRadius: 20).stroke(Color.orange, lineWidth: 4))
+    }
+    
+    var body: some View {
+        VStack {
+            verticalView
+            //acts like a spacer.
+            Color.white.frame(width: UIScreen.main.bounds.size.width, height: 20)
+            horizontalView
+        }
     }
 }
