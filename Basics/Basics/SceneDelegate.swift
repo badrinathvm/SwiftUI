14657c0bd919c4f48cf73231dc994dbe74d70baa
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-04-07 14:53:38 -0700

    Adoption of Swift Package Manager

--- Basics/Basics/SceneDelegate.swift
+++ Basics/Basics/SceneDelegate.swift
@@ -85,7 +85,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             //window.rootViewController = UIHostingController(rootView: FlipAnimationView())
             //window.rootViewController = UIHostingController(rootView: MultipleAnimationView())
             //window.rootViewController = UIHostingController(rootView: StepperAnimationView())
-            window.rootViewController = UIHostingController(rootView: StepDesignerView())
+            window.rootViewController = UIHostingController(rootView: StepTesterView())
             self.window = window
             window.makeKeyAndVisible()
         }
---
+++ Basics/Basics/View/Stepper/StepTesterView.swift
@@ -0,0 +1,44 @@
+//
+//  StepTesterView.swift
+//  Basics
+//
+//  Created by Venkatnarayansetty, Badarinath on 4/7/20.
+//  Copyright © 2020 Badarinath Venkatnarayansetty. All rights reserved.
+//
+
+import SwiftUI
+import StepperView
+
+struct StepTesterView: View {
+
+    let alignments = [StepperAlignment.top,StepperAlignment.center,StepperAlignment.bottom]
+
+    let indicatorTypes = [  StepperIndicationType<AnyView>.circle,
+                            StepperIndicationType.image(Image(systemName: "arrow.right.circle")),
+                            StepperIndicationType.image(Image(systemName: "arrow.uturn.right"))
+                        ]
+
+    var body: some View {
+          StepperView(cells: [cellBody, cellBody , cellBody], alignments: alignments,indicationType:indicatorTypes, verticalSpacing: 30.0)
+    }
+    
+    var cellBody:some View {
+        VStack(alignment: .leading) {
+            ForEach(["Top","Center","Bottom"], id:\.self) { value in
+                HStack {
+                    Text(value)
+                        .foregroundColor(Color.gray)
+                        .padding(.vertical , 10)
+                        .padding(.horizontal, 5)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
